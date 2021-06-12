cbf384f86b80eb1402a677f9873de98e49d1c56d
Amol Patil <amol.patil@capillarytech.com>
2021-06-12 21:18:51 +0530

    fixed condition

--- actions.py
+++ actions.py
@@ -164,7 +164,7 @@ def send_order(data, client_api_key, client_secret, is_test=True):
     if position_result[0]['size'] == '0' and position_result[1]['size'] == '0':
         print ("no position set")
         position_side = 'None'
-    elif position_side[0]['size'] == '0':
+    elif position_result[0]['size'] == '0':
         print ("position side is sell")
         position_side = "Sell"
     else:
